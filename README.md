478f9ec66202a59492c2970d721c963f64ac1af7
Yusuke Sugomori <yusuke_sugomori@micin.jp>
2019-03-07 21:12:50 +0900

    add logistic_regression_mnist

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# deeplearning-pytorch
-Deep Learning with PyTorch
+# Deep Learning with PyTorch 1.X
+
+Implementations of neural network models with torch (>=1.0)
+
+See also implementations with TesorFlow 2.0 [here](https://github.com/yusugomori/deeplearning-tf2).
+
+## Requirements
+
+* PyTorch >= 1.0
+
+```shell
+$ pip install torch torchvision
+```
---
+++ models/logistic_regression_mnist.py
@@ -0,0 +1,109 @@
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optimizers
+from torch.utils.data import Dataset, DataLoader
+import torchvision
+import torchvision.transforms as transforms
+from sklearn.metrics import accuracy_score
+
+
+class LogisticRegression(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(784, 10)
+
+    def forward(self, x):
+        x = self.linear(x)
+        y = torch.sigmoid(x)
+        return y
+
+
+if __name__ == '__main__':
+    np.random.seed(1234)
+    torch.manual_seed(1234)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    def compute_loss(label, pred):
+        return criterion(pred, label)
+
+    def train_step(x, t):
+        model.train(True)
+        preds = model(x)
+        loss = compute_loss(t, preds)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        return loss, preds
+
+    def test_step(x, t):
+        model.train(False)
+        preds = model(x)
+        loss = compute_loss(t, preds)
+
+        return loss, preds
+
+    '''
+    Load data
+    '''
+    root = os.path.join(os.path.dirname(__file__), '..', 'data')
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    lambda x: x.view(-1),
+                                    lambda x: x / 255.])
+    mnist_train = \
+        torchvision.datasets.MNIST(root=root,
+                                   download=True,
+                                   train=True,
+                                   transform=transform)
+    mnist_test = \
+        torchvision.datasets.MNIST(root=root,
+                                   download=True,
+                                   train=False,
+                                   transform=transform)
+
+    train_dataloader = DataLoader(mnist_train,
+                                  batch_size=100,
+                                  shuffle=True)
+    test_dataloader = DataLoader(mnist_test,
+                                 batch_size=100,
+                                 shuffle=False)
+
+    '''
+    Build model
+    '''
+    model = LogisticRegression().to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optimizers.Adam(model.parameters())
+
+    '''
+    Train model
+    '''
+    epochs = 10
+
+    for epoch in range(epochs):
+        train_loss = 0.
+        test_loss = 0.
+        test_acc = 0.
+
+        for (x, t) in train_dataloader:
+            loss, _ = train_step(x, t)
+            train_loss += loss.item()
+
+        train_loss /= len(train_dataloader)
+
+        if epoch % 5 == 4 or epoch == epochs - 1:
+            for (x, t) in test_dataloader:
+                loss, preds = test_step(x, t)
+                test_loss += loss.item()
+                test_acc += \
+                    accuracy_score(t, preds.argmax(dim=1))
+
+            test_loss /= len(test_dataloader)
+            test_acc /= len(test_dataloader)
+            print('Epoch: {}, Valid Cost: {:.3f}, Valid Acc: {:.3f}'.format(
+                epoch+1,
+                test_loss,
+                test_acc
+            ))
