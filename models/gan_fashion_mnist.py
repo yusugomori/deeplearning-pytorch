fb635d3892f3f67e2a6d1d1f9da41df951ed56ee
Yusuke Sugomori <yusuke_sugomori@micin.jp>
2019-03-26 16:40:21 +0900

    update gan

--- models/gan_fashion_mnist.py
+++ models/gan_fashion_mnist.py
@@ -34,68 +34,79 @@ class GAN(nn.Module):
             p.requires_grad = trainable
 
 
+class Discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 256,
+                               kernel_size=(3, 3),
+                               stride=(2, 2),
+                               padding=1)
+        self.relu1 = nn.LeakyReLU(0.2)
+        self.dropout1 = nn.Dropout(0.3)
+        self.conv2 = nn.Conv2d(256, 512,
+                               kernel_size=(3, 3),
+                               stride=(2, 2),
+                               padding=1)
+        self.bn2 = nn.BatchNorm2d(512)
+        self.relu2 = nn.LeakyReLU(0.2)
+        self.dropout2 = nn.Dropout(0.3)
+        self.fc = nn.Linear(512*7*7, 1024)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.relu3 = nn.LeakyReLU(0.2)
+        self.out = nn.Linear(1024, 1)
+
+    def forward(self, x):
+        h = self.conv1(x)
+        h = self.relu1(h)
+        h = self.dropout1(h)
+        h = self.conv2(h)
+        h = self.bn2(h)
+        h = self.relu2(h)
+        h = self.dropout2(h)
+        h = h.view(-1, 512*7*7)
+        h = self.fc(h)
+        h = self.bn3(h)
+        h = self.relu3(h)
+        h = self.out(h)
+        y = torch.sigmoid(h)
+
+        return y
+
+
 class Generator(nn.Module):
     def __init__(self,
-                 input_dim=128):
+                 input_dim=100):
         super().__init__()
         self.linear = nn.Linear(input_dim, 256*14*14)
         self.bn1 = nn.BatchNorm1d(256*14*14)
+        self.relu1 = nn.ReLU()
         self.conv1 = nn.Conv2d(256, 128,
                                kernel_size=(3, 3),
                                padding=1)
         self.bn2 = nn.BatchNorm2d(128)
+        self.relu2 = nn.ReLU()
         self.conv2 = nn.Conv2d(128, 64,
                                kernel_size=(3, 3),
                                padding=1)
         self.bn3 = nn.BatchNorm2d(64)
+        self.relu3 = nn.ReLU()
         self.conv3 = nn.Conv2d(64, 1,
                                kernel_size=(1, 1))
 
     def forward(self, x):
         h = self.linear(x)
         h = self.bn1(h)
-        h = torch.relu(h)
+        h = self.relu1(h)
         h = h.view(-1, 256, 14, 14)
         h = nn.functional.interpolate(h, size=(28, 28))
         h = self.conv1(h)
         h = self.bn2(h)
-        h = torch.relu(h)
+        h = self.relu2(h)
         h = self.conv2(h)
         h = self.bn3(h)
-        h = torch.relu(h)
+        h = self.relu3(h)
         h = self.conv3(h)
         y = torch.sigmoid(h)
-
-        return y
-
-
-class Discriminator(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(1, 256,
-                               kernel_size=(3, 3),
-                               padding=1)
-        self.dropout1 = nn.Dropout(0.3)
-        self.conv2 = nn.Conv2d(256, 512,
-                               kernel_size=(3, 3),
-                               padding=1)
-        self.dropout2 = nn.Dropout(0.3)
-        self.avg_pool = GlobalAvgPool2d()
-        self.fc = nn.Linear(512, 1000)
-        self.out = nn.Linear(1000, 2)
-
-    def forward(self, x):
-        h = self.conv1(x)
-        h = torch.relu(h)
-        h = self.dropout1(h)
-        h = self.conv2(h)
-        h = torch.relu(h)
-        h = self.dropout2(h)
-        h = self.avg_pool(h)
-        h = self.fc(h)
-        h = torch.relu(h)
-        y = self.out(h)
-        y = torch.log_softmax(h, dim=-1)
 
         return y
 
@@ -108,35 +119,39 @@ if __name__ == '__main__':
     def compute_loss(label, pred):
         return criterion(pred, label)
 
-    def train_step_D(x):
-        model.set_trainable(model.D, True)
-        model.train()
-        noise = gen_noise(x.size(0))
-        gen = model.G(noise)
-        x = torch.cat((x, gen))
-        t = torch.zeros(x.size(0)).long().to(device)
-        t[:t.size(0)//2] = 1
+    def train_step(x):
+        batch_size = x.size(0)
+        model.D.train()
+        model.G.train()
 
-        preds = model.D(x)
-        loss = compute_loss(t, preds)
+        # train D
+        # real images
+        preds = model.D(x).squeeze()  # preds with true images
+        t = torch.ones(batch_size).float().to(device)
+        loss_D_real = compute_loss(t, preds)
+        # fake images
+        noise = gen_noise(batch_size)
+        z = model.G(noise)
+        preds = model.D(z.detach()).squeeze()  # preds with fake images
+        t = torch.zeros(batch_size).float().to(device)
+        loss_D_fake = compute_loss(t, preds)
+
+        loss_D = loss_D_real + loss_D_fake
         optimizer_D.zero_grad()
-        loss.backward()
+        loss_D.backward()
         optimizer_D.step()
 
-        return loss, preds
-
-    def train_step_G():
-        model.set_trainable(model.D, False)
-        model.train()
-        noise = gen_noise(x.size(0))
-        t = torch.ones(x.size(0)).long().to(device)
-        preds = model(noise)
-        loss = compute_loss(t, preds)
+        # train G
+        noise = gen_noise(batch_size)
+        z = model.G(noise)
+        preds = model.D(z).squeeze()  # preds with fake images
+        t = torch.ones(batch_size).float().to(device)  # label as true
+        loss_G = compute_loss(t, preds)
         optimizer_G.zero_grad()
-        loss.backward()
+        loss_G.backward()
         optimizer_G.step()
 
-        return loss, preds
+        return loss_D, loss_G
 
     def generate(batch_size=10):
         model.eval()
@@ -146,12 +161,13 @@ if __name__ == '__main__':
         return gen
 
     def gen_noise(batch_size):
-        return torch.empty(batch_size, 128).uniform_(0, 1).to(device)
+        return torch.empty(batch_size, 100).uniform_(0, 1).to(device)
 
     '''
     Load data
     '''
-    root = os.path.join(os.path.dirname(__file__), '..', 'data')
+    root = os.path.join(os.path.dirname(__file__),
+                        '..', 'data', 'fashion_mnist')
     transform = transforms.Compose([transforms.ToTensor(),
                                     lambda x: x / 255.])
     mnist_train = \
@@ -159,31 +175,22 @@ if __name__ == '__main__':
                                           download=True,
                                           train=True,
                                           transform=transform)
-    mnist_test = \
-        torchvision.datasets.FashionMNIST(root=root,
-                                          download=True,
-                                          train=False,
-                                          transform=transform)
-
     train_dataloader = DataLoader(mnist_train,
                                   batch_size=100,
                                   shuffle=True)
-    test_dataloader = DataLoader(mnist_test,
-                                 batch_size=100,
-                                 shuffle=False)
 
     '''
     Build model
     '''
     model = GAN().to(device)
-    criterion = nn.NLLLoss()
-    optimizer_G = optimizers.Adam(model.G.parameters())
+    criterion = nn.BCELoss()
     optimizer_D = optimizers.Adam(model.D.parameters())
+    optimizer_G = optimizers.Adam(model.G.parameters())
 
     '''
     Train model
     '''
-    epochs = 10000
+    epochs = 100
     out_path = os.path.join(os.path.dirname(__file__),
                             '..', 'output')
 
@@ -194,12 +201,10 @@ if __name__ == '__main__':
 
         for (x, _) in train_dataloader:
             x = x.to(device)
+            loss_D, loss_G = train_step(x)
 
-            loss, _ = train_step_D(x)  # train D
-            train_loss_D += loss.item()
-
-            loss, _ = train_step_G()  # train G
-            train_loss_G += loss.item()
+            train_loss_D += loss_D.item()
+            train_loss_G += loss_G.item()
 
         train_loss_D /= len(train_dataloader)
         train_loss_G /= len(train_dataloader)
@@ -210,15 +215,15 @@ if __name__ == '__main__':
             train_loss_G
         ))
 
-        # Generate images
-        if epoch % 10 == 9 or epoch == epochs - 1:
-            images = generate(batch_size=10)
+        if epoch % 5 == 4 or epoch == epochs - 1:
+            images = generate(batch_size=16)
             images = images.squeeze().detach().cpu().numpy()
-            plt.figure(figsize=(8, 4))
+            plt.figure(figsize=(6, 6))
             for i, image in enumerate(images):
-                plt.subplot(2, 5, i+1)
+                plt.subplot(4, 4, i+1)
                 plt.imshow(image, cmap='binary')
                 plt.axis('off')
             plt.tight_layout()
+            # plt.show()
             template = '{}/gan_fashion_mnist_epoch_{:0>4}.png'
             plt.savefig(template.format(out_path, epoch), dpi=300)
